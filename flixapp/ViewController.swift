d75e03106db23e6bda3e7d8545c50fc0fe01de28
Vasu Ramanujam <vasur@umich.edu>
2022-09-19 09:51:02 -0400

    Initial Commit

---
+++ flixapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixapp
+//
+//  Created by Srivasudha Ramanujam on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
