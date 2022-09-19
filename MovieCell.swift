6494336597b53eac48276cd2329306e0ad3ba313
Vasu Ramanujam <vasur@umich.edu>
2022-09-19 16:32:46 -0400

    halfway thru wk 1 babes
    um idrk whats going on. need to download cocoa pod. halfway thru wk1

---
+++ MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flixapp
+//
+//  Created by Srivasudha Ramanujam on 9/19/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
