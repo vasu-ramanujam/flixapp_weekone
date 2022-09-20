cb806aa216a9b1c04ee3864e1db6fc49dcd61dd7
Vasu Ramanujam <vasur@umich.edu>
2022-09-19 21:27:08 -0400

    finished w1

--- flixapp/MoviesViewController.swift
+++ flixapp/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -62,9 +63,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
-        
-        
-
+        cell.posterView.af_setImage(withURL: posterUrl!)
         
         return cell;
     }
